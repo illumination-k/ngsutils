b2e03b2ae2c6e5a5993eb3d36cbd44d6cbbf0a5d
illumination-k <illumination.k.27@gmail.com>
2020-01-15 08:56:54 +0000

    update read_featureCounts.py

--- ngsutils/reader/read_featureCounts.py
+++ ngsutils/reader/read_featureCounts.py
@@ -6,5 +6,15 @@ import pandas as pd
 logger = logging.getLogger(__name__)
 
 
-def read_featureCounts(path):
-    pass
+def read_featureCounts(path_to_featureCounts_data: str, path_to_conditions: str="") -> anndata.core.anndata.AnnData:
+    featureCounts_df = pd.read_csv(path_to_featureCounts_data, sep="\t", skiprows=1, index_col=0)
+    if path_to_conditions != "":
+        conditions_df = pd.read_csv(path_to_conditions, sep=",", index_col=0)
+        adata = anndata.AnnData(featureCounts_df.drop(['Chr', 'Start', 'End', 'Strand', 'Length'], axis=1),
+                                obs=featureCounts_df[['Chr', 'Start', 'End', 'Strand', 'Length']]).T
+        adata.obs = conditions_df
+    else:
+        print("WARNING: If you want to DEG analysis, please load conditions")
+        adata = anndata.AnnData(featureCounts_df.drop(['Chr', 'Start', 'End', 'Strand', 'Length'], axis=1),
+                                obs=featureCounts_df[['Chr', 'Start', 'End', 'Strand', 'Length']]).T
+    return adata
